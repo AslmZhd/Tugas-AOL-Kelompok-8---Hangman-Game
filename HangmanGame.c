7004aeb401c0c58ec21b37f50fb6630e5b9030a0
Albert-abc <albertbcsecondacc@gmail.com>
2026-01-01 11:27:01 +0700

    Update HangmanGame.c

--- HangmanGame.c
+++ HangmanGame.c
@@ -1,1 +1,14 @@
+int choice;
 
+do{
+  printf("\n1. Play Game\n2. View High Scores\n3. Exit\n");
+  printf("Choose: ");
+  scanf("%d", &choice);
+}
+
+switch (choice) {
+  case 1: playGame(); break;
+  case 2: showScores(); break;
+  case 3: printf("Goodbye!\n"); break;
+  default: printf("Invalid choice!\n");
+} while (choice != 3);
